eb092dc77a74ee6930656ae9c5612025ae70e829
wyx2685 <wyx2685@gmail.com>
2025-05-25 20:35:39 +0900

    test: anytls节点订阅

--- app/Protocols/ClashNyanpasu.php
+++ app/Protocols/ClashNyanpasu.php
@@ -56,6 +56,10 @@ class ClashNyanpasu
                 array_push($proxy, self::buildTuic($user['uuid'], $item));
                 array_push($proxies, $item['name']);
             }
+            if ($item['type'] === 'anytls') {
+                array_push($proxy, self::buildAnyTLS($user['uuid'], $item));
+                array_push($proxies, $item['name']);
+            }
             if ($item['type'] === 'hysteria') {
                 array_push($proxy, self::buildHysteria($user['uuid'], $item));
                 array_push($proxies, $item['name']);
@@ -295,6 +299,30 @@ class ClashNyanpasu
             $array['sni'] = $server['server_name'];
         }
 
+        return $array;
+    }
+
+    public static function buildAnyTLS($password, $server)
+    {
+        $array = [
+            'name' => $server['name'],
+            'type' => 'anytls',
+            'server' => $server['host'],
+            'port' => $server['port'],
+            'password' => $password,
+            'client-fingerprint' => 'chrome',
+            'udp' => true,
+            'alpn' => [
+                'h2',
+                'http/1.1',
+            ],
+        ];
+        if (isset($server['server_name'])) {
+            $array['sni'] = $server['server_name'];
+        }
+        if ($server['insecure']) {
+            $array['skip-cert-verify'] = true;
+        }
         return $array;
     }
 
