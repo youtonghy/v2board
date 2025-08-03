0ccb8868772b981648c1bbb04a20b12e80b1005e
wyx2685 <wyx2685@gmail.com>
2025-08-03 20:40:59 +0900

    fix: 临时生效的订阅链接无法绑定telegram

--- app/Plugins/Telegram/Commands/Bind.php
+++ app/Plugins/Telegram/Commands/Bind.php
@@ -4,6 +4,8 @@ namespace App\Plugins\Telegram\Commands;
 
 use App\Models\User;
 use App\Plugins\Telegram\Telegram;
+use App\Utils\Helper;
+use Illuminate\Support\Facades\Cache;
 
 class Bind extends Telegram {
     public $command = '/bind';
@@ -20,6 +22,45 @@ class Bind extends Telegram {
         $token = $query['token'];
         if (!$token) {
             abort(500, '订阅地址无效');
+        }
+        $submethod = (int)config('v2board.show_subscribe_method', 0);
+        switch ($submethod) {
+            case 0:
+                break;
+            case 1:
+                if (!Cache::has("otpn_{$token}")) {
+                    abort(403, 'token is error');
+                }
+                $usertoken = Cache::get("otpn_{$token}");
+                $token = $usertoken;
+                break;
+            case 2:
+                $usertoken = Cache::get("totp_{$token}");
+                if (!$usertoken) {
+                    $timestep = (int)config('v2board.show_subscribe_expire', 5) * 60;
+                    $counter = floor(time() / $timestep);
+                    $counterBytes = pack('N*', 0) . pack('N*', $counter);
+                    $idhash = Helper::base64DecodeUrlSafe($token);
+                    $parts = explode(':', $idhash, 2);
+                    [$userid, $clienthash] = $parts;
+                    if (!$userid || !$clienthash) {
+                        abort(403, 'token is error');
+                    }
+                    $user = User::where('id', $userid)->select('token')->first();
+                    if (!$user) {
+                        abort(403, 'token is error');
+                    }
+                    $usertoken = $user->token;
+                    $hash = hash_hmac('sha1', $counterBytes, $usertoken, false);
+                    if ($clienthash !== $hash) {
+                        abort(403, 'token is error');
+                    }
+                    Cache::put("totp_{$token}", $usertoken, $timestep);
+                }
+                $token = $usertoken;
+                break;
+            default:
+                break;
         }
         $user = User::where('token', $token)->first();
         if (!$user) {
