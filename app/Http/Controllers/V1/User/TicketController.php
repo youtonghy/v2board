ad4ee964b5ac2db8004d24e3c1e6a434dd9580aa
codeman857 <58798231+codeman857@users.noreply.github.com>
2025-08-13 21:09:39 +0800

    fix: 修复工单转发到 Telegram 内容因错误的 markdown 标记造成的内容中 markdown 语法无法正常渲染问题 (#271)

--- app/Http/Controllers/V1/User/TicketController.php
+++ app/Http/Controllers/V1/User/TicketController.php
@@ -14,7 +14,7 @@ use App\Services\TicketService;
 use App\Models\Ticket;
 use App\Models\TicketMessage;
 use App\Utils\Dict;
-use Illuminate\Http\Request; 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class TicketController extends Controller
@@ -23,7 +23,7 @@ class TicketController extends Controller
     {
         $userId = $request->user['id'];
         $ticketId = $request->input('id');
-    
+
         if ($ticketId) {
             $ticket = Ticket::where('id', $ticketId)
                 ->where('user_id', $userId)
@@ -37,9 +37,9 @@ class TicketController extends Controller
                     $ticket['message'][$i]['is_me'] = true;
                 }
             }
-							 
+
             return response(['data' => $ticket]);
-			   
+
         }
         $ticket = Ticket::where('user_id', $userId)
             ->orderBy('created_at', 'DESC')
@@ -69,7 +69,7 @@ class TicketController extends Controller
                     $hasOrder = Order::where('user_id', $request->user['id'])
                         ->whereIn('status', [3, 4])
                         ->exists();
-                
+
                     if (!$hasOrder) {
                         throw new \Exception(__('请先购买套餐'));
                     }
@@ -82,10 +82,10 @@ class TicketController extends Controller
                     // 处理未知状态
                     throw new \Exception(__('未知的工单状态'));
             }
-															 
+
             $ticketData = $request->only(['subject', 'level']) + ['user_id' => $request->user['id']];
             $ticket = Ticket::create($ticketData);
-    
+
             TicketMessage::create([
                 'user_id' => $request->user['id'],
                 'ticket_id' => $ticket->id,
@@ -177,7 +177,7 @@ class TicketController extends Controller
 				$request->input('withdraw_method'),
 				config(
 					'v2board.commission_withdraw_method',
-					Dict::WITHDRAW_METHOD_WHITELIST_DEFAULT	 
+					Dict::WITHDRAW_METHOD_WHITELIST_DEFAULT
 				)
 			)
 		) {
@@ -225,7 +225,7 @@ class TicketController extends Controller
 		$telegramService = new TelegramService();
 		if (!empty($userid)) {
 			$user = User::find($userid);
-			
+
 			if ($user) {
 				$transfer_enable = $this->getFlowData($user->transfer_enable); // 总流量
 				$remaining_traffic = $this->getFlowData($user->transfer_enable - $user->u - $user->d); // 剩余流量
@@ -239,7 +239,7 @@ class TicketController extends Controller
 				} else {
 					$ip_address = $_SERVER['REMOTE_ADDR'];
 				}
-				
+
 				$api_url = "http://ip-api.com/json/{$ip_address}?fields=520191&lang=zh-CN";
 				$response = file_get_contents($api_url);
 				$user_location = json_decode($response, true);
@@ -248,19 +248,19 @@ class TicketController extends Controller
 				} else {
 					$location =  "无法确定用户地址";
 				}
-				
+
 				$plan = Plan::where('id', $user->plan_id)->first();
 				$planName = $plan ? $plan->name : '未找到套餐信息'; // Check if plan data is available
-				
+
 				$money = $user->balance / 100;
 				$affmoney = $user->commission_balance / 100;
-				$telegramService->sendMessageWithAdmin("📮工单提醒 #{$ticket->id}\n———————————————\n邮箱：\n`{$user->email}`\n用户位置：\n`{$location}`\nIP:\n{$ip_address}\n套餐与流量：\n`{$planName} of {$transfer_enable}/{$remaining_traffic}`\n上传/下载：\n`{$u}/{$d}`\n到期时间：\n`{$expired_at}`\n余额/佣金余额：\n`{$money}/{$affmoney}`\n主题：\n`{$ticket->subject}`\n内容：\n`{$message}`", true);
+				$telegramService->sendMessageWithAdmin("📮工单提醒 #{$ticket->id}\n———————————————\n邮箱：\n`{$user->email}`\n用户位置：\n`{$location}`\nIP:\n{$ip_address}\n套餐与流量：\n`{$planName} of {$transfer_enable}/{$remaining_traffic}`\n上传/下载：\n`{$u}/{$d}`\n到期时间：\n`{$expired_at}`\n余额/佣金余额：\n`{$money}/{$affmoney}`\n主题：\n`{$ticket->subject}`\n内容：\n {$message} ", true);
 			} else {
 				// Handle case where user data is not found
 				$telegramService->sendMessageWithAdmin("User data not found for user ID: {$userid}", true);
 			}
 		} else {
-			$telegramService->sendMessageWithAdmin("📮工单提醒 #{$ticket->id}\n———————————————\n主题：\n`{$ticket->subject}`\n内容：\n`{$message}`", true);
+			$telegramService->sendMessageWithAdmin("📮工单提醒 #{$ticket->id}\n———————————————\n主题：\n`{$ticket->subject}`\n内容：\n {$message} ", true);
 		}
 	}
 
