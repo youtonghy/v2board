94a1d1f49507450b58027264eea4ef69e13fb64f
CA <sechk777@gmail.com>
2025-11-21 20:23:43 +0800

    test: 路由规则增加禁止访问端口目标 (#292)

--- public/assets/admin/umi.js
+++ public/assets/admin/umi.js
@@ -100722,6 +100722,7 @@
             routeActionText: {
                 block: "\u7981\u6b62\u8bbf\u95ee(\u57df\u540d\u76ee\u6807)",
                 block_ip: "\u7981\u6b62\u8bbf\u95ee(IP\u76ee\u6807)",
+                block_port: "\u7981\u6b62\u8bbf\u95ee(\u7aef\u53e3\u76ee\u6807)",
                 protocol: "\u7981\u6b62\u8bbf\u95ee(\u534f\u8bae)",
                 dns: "\u6307\u5b9aDNS\u670d\u52a1\u5668\u8fdb\u884c\u89e3\u6790",
                 route: "\u6307\u5b9a\u51fa\u7ad9\u670d\u52a1\u5668(\u57df\u540d\u76ee\u6807)",
@@ -111208,6 +111209,9 @@
                         if (action === "protocol") {
                             return "http\ntls\nquic\nbittorrent";
                         }
+                        if (action === "block_port") {
+                            return "53\n443\n1000-2000";
+                        }
                         if (["route_ip", "block_ip"].includes(action)) {
                             return "127.0.0.1(\u5355\u4e00\u5339\u914d)\n10.0.0.0/8(\u8303\u56f4\u5339\u914d)\ngeoip:cn(\u9884\u5b9a\u4e49\u5217\u8868\u5339\u914d)";
                         }
@@ -111242,6 +111246,8 @@
                 }, b["a"].routeActionText["block"]), f.a.createElement(v["a"].Option, {
                     value: "block_ip"
                 }, b["a"].routeActionText["block_ip"]), f.a.createElement(v["a"].Option, {
+                    value: "block_port"
+                }, b["a"].routeActionText["block_port"]), f.a.createElement(v["a"].Option, {
                     value: "protocol"
                 }, b["a"].routeActionText["protocol"]), f.a.createElement(v["a"].Option, {
                     value: "dns"
